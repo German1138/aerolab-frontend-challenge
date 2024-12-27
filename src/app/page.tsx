7c860f23e7704242a795da433ca67228431ace64
German1138 <germancourvoisier1138@gmail.com>
2024-12-27 18:48:49 -0300

    refactor: :construction: Changed Home component WIP
    Now it displays in a very basic way the game covers, filters and searchbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,71 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import { Container, Grid2, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Image from "next/image";
+import Searchbar from "@/components/Searchbar/Searchbar";
+import Filters from "@/components/Filters/Filters";
+import customImageUrl from "@/utils/customImageUrl";
+import Link from "next/link";
+import { IGame } from "./interfaces";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [games, setGames] = useState<IGame[]>([]);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const loadInitialData = async () => {
+    try {
+      const { data } = await axios.get("/api/games");
+      setGames(data);
+    } catch (error) {
+      console.error(error);
+      setGames([]);
+    }
+  };
+
+  useEffect(() => {
+    loadInitialData();
+  }, []);
+
+  return (
+    <Container>
+      <Typography variant="h1" sx={{ fontSize: "20px", fontWeight: "600" }}>
+        Gaming Haven Z
+      </Typography>
+
+      <Searchbar />
+
+      <Typography variant="h3" sx={{ fontSize: "20px", fontWeight: "600" }}>
+        Saved Games
+      </Typography>
+
+      <Filters />
+
+      <Grid2
+        container
+        spacing={2}
+        sx={{ display: "flex", gap: "10px", flexWrap: "wrap" }}
+      >
+        {games?.map((game: IGame) => {
+          return (
+            <Grid2
+              size={4}
+              key={game.id}
+              sx={{ margin: "40px 0", borderRadius: "8px" }}
+            >
+              <Link href={`/detail/${game.slug}`}>
+                <Image
+                  src={customImageUrl("cover_big", game.cover?.image_id)}
+                  width={115}
+                  height={155}
+                  alt={`${game.name} cover`}
+                />
+              </Link>
+            </Grid2>
+          );
+        })}
+      </Grid2>
+    </Container>
   );
 }
