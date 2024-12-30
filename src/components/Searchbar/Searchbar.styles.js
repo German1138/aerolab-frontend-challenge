aa43a69b4f0f0760698c45f633b387429dca02ab
German1138 <germancourvoisier1138@gmail.com>
2024-12-30 10:23:43 -0300

    feat: :boom: Overhaul of searchbar (WIP)
    Practically done. Lacks the logic for the form when it's submited and the error handling.

--- src/components/Searchbar/Searchbar.styles.js
+++ src/components/Searchbar/Searchbar.styles.js
@@ -1,25 +1,26 @@
 export const container = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  width: '100%',
-  maxWidth: '700px',
-  margin: '0 10px',
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+  maxWidth: "700px",
+  margin: "0 10px",
 };
 
 export const textField = {
-  backgroundColor: '#D4D4D4',
-  borderRadius: '100px',
-  border: '0px solid',
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      border: 'none',
+  backgroundColor: "#FFFFFF",
+  borderRadius: "100px",
+  width: "100%",
+  border: "1px solid #C698B8",
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      border: "none",
     },
-    '&.Mui-focused fieldset': {
-      border: 'none',
+    "&.Mui-focused fieldset": {
+      border: "none",
     },
-    '&:hover fieldset': {
-      border: 'none',
+    "&:hover fieldset": {
+      border: "none",
     },
   },
 };
--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,29 +1,100 @@
-import { Box, InputAdornment, TextField } from "@mui/material";
+"use client";
+
+import { Autocomplete, Box, InputAdornment, TextField } from "@mui/material";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import React, { useState } from "react";
+import axios from "axios";
+import customImageUrl from "@/utils/customImageUrl";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-import React from "react";
-import { container, textField } from "./Searchbar.styles";
-
-function Searchbar() {
-  return (
-    <Box sx={container}>
-      <TextField
-        placeholder="Search for a topic..."
-        variant="outlined"
-        fullWidth
-        slotProps={{
-          input: {
-            endAdornment: (
-              <InputAdornment position="start">
-                <SearchOutlinedIcon />
-              </InputAdornment>
-            ),
-          },
-        }}
-        sx={textField}
-      />
-    </Box>
-  );
+interface Option {
+  id: string;
+  name: string;
+  slug: string;
+  cover?: {
+    image_id: string;
+  };
 }
+
+const fetchSearchResults = async (query: string): Promise<Option[]> => {
+  try {
+    const { data } = await axios.get(`/api/search?value=${query}`);
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+const Searchbar = () => {
+  const [options, setOptions] = useState<Option[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleInputChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+
+    if (value.length >= 3) {
+      const results = await fetchSearchResults(value);
+      setOptions(results);
+    } else {
+      setOptions([]);
+    }
+  };
+
+  const renderOption = (
+    props: React.HTMLAttributes<HTMLAnchorElement>,
+    option: Option
+  ) => (
+    <Link
+      {...props}
+      href={`/detail/${option.slug}`}
+      key={option.id}
+      style={{ display: "flex", alignItems: "center" }}
+    >
+      <img
+        src={customImageUrl("micro", option.cover?.image_id)}
+        alt={option.name}
+        style={{ width: 30, height: 30, marginRight: 10 }}
+      />
+      {option.name}
+    </Link>
+  );
+
+  return (
+    <form style={{ width: "100%", margin: "auto" }}>
+      <Autocomplete
+        freeSolo
+        options={options}
+        inputValue={searchTerm}
+        onInputChange={(_, newInputValue) => setSearchTerm(newInputValue)}
+        getOptionLabel={(option) =>
+          typeof option === "string" ? option : option.name || ""
+        }
+        renderOption={renderOption}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            placeholder="Search games..."
+            variant="outlined"
+            fullWidth
+            onChange={handleInputChange}
+            InputProps={{
+              ...params.InputProps,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchOutlinedIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        )}
+      />
+    </form>
+  );
+};
 
 export default Searchbar;
