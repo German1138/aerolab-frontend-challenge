8b12fe7d142cf98acddba904479110b1c545b84f
German1138 <germancourvoisier1138@gmail.com>
2024-12-30 10:16:51 -0300

    refactor: :recycle: Filters renders directly in HomeScreen
    This due to the imposibility to send the state to the desire component. May refactor again in the future.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,27 @@
 "use client";
 
-import styles from "./page.module.css";
-import { Container, Grid2, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Image from "next/image";
+import { Box, Button, Container, Typography } from "@mui/material";
 import Searchbar from "@/components/Searchbar/Searchbar";
-import Filters from "@/components/Filters/Filters";
-import customImageUrl from "@/utils/customImageUrl";
-import Link from "next/link";
-import { IGame } from "./interfaces";
+import GameCard from "@/components/GameCard";
+
+import { useState } from "react";
+import {
+  container,
+  btnStylesMobile,
+} from "../components/Filters/Filters.styles";
 
 export default function Home() {
-  const [games, setGames] = useState<IGame[]>([]);
+  const [filter, setFilter] = useState<string>("last-added");
 
-  const loadInitialData = async () => {
-    try {
-      const { data } = await axios.get("/api/games");
-      setGames(data);
-    } catch (error) {
-      console.error(error);
-      setGames([]);
-    }
-  };
-
-  useEffect(() => {
-    loadInitialData();
-  }, []);
-
+  const buttonData = [
+    { name: "last-added", label: "Last Added", id: 1 },
+    { name: "newest", label: "Newest", id: 2 },
+    {
+      name: "oldest",
+      label: "Oldest",
+      id: 3,
+    },
+  ];
   return (
     <Container>
       <Typography variant="h1" sx={{ fontSize: "20px", fontWeight: "600" }}>
@@ -40,32 +34,26 @@ export default function Home() {
         Saved Games
       </Typography>
 
-      <Filters />
+      <Box sx={container}>
+        {buttonData.map(({ name, label, id }) => (
+          <Button
+            key={id}
+            variant={filter === name ? "contained" : "text"}
+            onClick={() => setFilter(name)}
+            sx={{
+              ...btnStylesMobile,
+              ...(filter === name && {
+                backgroundColor: "#3C1661",
+                color: "#FFFFFF",
+              }),
+            }}
+          >
+            {label}
+          </Button>
+        ))}
+      </Box>
 
-      <Grid2
-        container
-        spacing={2}
-        sx={{ display: "flex", gap: "10px", flexWrap: "wrap" }}
-      >
-        {games?.map((game: IGame) => {
-          return (
-            <Grid2
-              size={4}
-              key={game.id}
-              sx={{ margin: "40px 0", borderRadius: "8px" }}
-            >
-              <Link href={`/detail/${game.slug}`}>
-                <Image
-                  src={customImageUrl("cover_big", game.cover?.image_id)}
-                  width={115}
-                  height={155}
-                  alt={`${game.name} cover`}
-                />
-              </Link>
-            </Grid2>
-          );
-        })}
-      </Grid2>
+      <GameCard filter={filter} />
     </Container>
   );
 }
