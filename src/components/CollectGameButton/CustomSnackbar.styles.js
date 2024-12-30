9012737daf71e53234228129b4493b12bdfe2ce9
German1138 <germancourvoisier1138@gmail.com>
2024-12-30 18:11:57 -0300

    feat: :sparkles: New customSnackbar component
    It serves as the notification when we add a new game to the collection, it can be used anywhere.

---
+++ src/components/CollectGameButton/CustomSnackbar.styles.js
@@ -0,0 +1,18 @@
+export const snackbarContainer = {
+  borderRadius: "8px",
+  backgroundColor: "#FFFFFF",
+  width: "100%",
+  padding: "16px",
+  marginBottom: "40px",
+};
+export const titleContainer = {
+  display: "flex",
+  alignItems: "center",
+  marginBottom: "5px",
+};
+
+export const titleStyle = {
+  fontWeight: 600,
+  marginLeft: "5px",
+};
+export const messageStyle = { fontSize: 14, color: "#666666" };
---
+++ src/components/CollectGameButton/CustomSnackbar.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Box, Snackbar, Typography } from "@mui/material";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import { IGame, IGameDetail } from "@/app/interfaces";
+import {
+  messageStyle,
+  snackbarContainer,
+  titleContainer,
+  titleStyle,
+} from "./CustomSnackbar.styles";
+
+interface ISnackbarProps {
+  game: IGame | IGameDetail;
+  clicked: boolean;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function CustomSnackbar({ game, clicked, open, setOpen }: ISnackbarProps) {
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ): void => {
+    if (reason !== "clickaway") {
+      setOpen(false);
+    }
+  };
+
+  const borderColor = clicked ? "#67C076" : "#D23F63";
+  const message = clicked
+    ? "has been added to your collection"
+    : "has been removed from your collection";
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      open={open}
+      autoHideDuration={2000}
+      onClose={handleClose}
+    >
+      <Box
+        sx={{
+          ...snackbarContainer,
+          border: `1px solid ${borderColor}`,
+        }}
+      >
+        <Box sx={titleContainer}>
+          {clicked ? (
+            <CheckCircleOutlineIcon style={{ color: "#67C076" }} />
+          ) : (
+            <ErrorOutlineIcon style={{ color: "#D23F63" }} />
+          )}
+          <Typography sx={titleStyle}>
+            {clicked ? "Game collected" : "Game removed"}
+          </Typography>
+        </Box>
+        <Typography sx={messageStyle}>
+          {game.name} {message}
+        </Typography>
+      </Box>
+    </Snackbar>
+  );
+}
+
+export default CustomSnackbar;
