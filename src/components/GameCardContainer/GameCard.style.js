1c2f5fd14d7165f802f649181eedce42738bbd2c
German1138 <germancourvoisier1138@gmail.com>
2024-12-30 17:39:49 -0300

    feat: :sparkles: New reusable component GameCard
    GameCard will return a grid prepared component that's only an image. It can be clicked and will redirect you to the detail of the game. Also we can specify if we want the Delete from collection button.

---
+++ src/components/GameCardContainer/GameCard.style.js
@@ -0,0 +1,21 @@
+export const iconButtonStyles = {
+  backgroundColor: "#FFFFFF",
+  position: "absolute",
+  height: "40px",
+  bottom: 12,
+  right: 15,
+  ":hover": {
+    background: "#FFFFFF",
+    filter: "brightness(130%)",
+    boxShadow: "1px 1px 6px",
+  },
+};
+
+export const subContainer = { position: "relative", width: "fit-content" };
+
+export const cardStyle = {
+  maxWidth: 170,
+  width: "fit-content",
+  cursor: "pointer",
+  borderRadius: "8px",
+};
---
+++ src/components/GameCardContainer/GameCard.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useState, useCallback } from "react";
+import customImageUrl from "@/utils/customImageUrl";
+import handleLS from "@/utils/handleLS";
+import { Box, Grid2, Card, CardMedia, IconButton } from "@mui/material";
+import Link from "next/link";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { loadInitialData } from "./GameCardContainer";
+import { cardStyle, iconButtonStyles, subContainer } from "./GameCard.style";
+import { IGame } from "@/app/interfaces";
+import CustomSnackbar from "../CollectGameButton/CustomSnackbar";
+
+interface IGameCard {
+  game: IGame;
+  filter?: string;
+  setGames?: React.Dispatch<React.SetStateAction<IGame[]>>;
+  disableIconButton?: boolean;
+}
+
+function GameCard({
+  game,
+  filter = "last-added",
+  setGames,
+  disableIconButton = false,
+}: IGameCard) {
+  const [clicked, setClicked] = useState(true);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setClicked((prev) => !prev);
+    setOpen(handleLS(game));
+    if (setGames) {
+      loadInitialData(setGames, filter);
+    }
+  }, [game, filter, setGames]);
+
+  const gameImageUrl = useCallback(
+    () => customImageUrl("cover_big", game.cover?.image_id),
+    [game.cover?.image_id]
+  );
+
+  return (
+    <Grid2 size={4} key={game.id}>
+      <Box sx={subContainer}>
+        <Link href={`/detail/${game.slug}`} passHref>
+          <Card sx={cardStyle}>
+            <CardMedia
+              component="img"
+              image={gameImageUrl()}
+              alt={`${game.name} cover`}
+            />
+          </Card>
+        </Link>
+
+        {!disableIconButton && (
+          <IconButton onClick={handleClick} sx={iconButtonStyles}>
+            <DeleteIcon />
+          </IconButton>
+        )}
+
+        <CustomSnackbar
+          game={game}
+          clicked={clicked}
+          open={open}
+          setOpen={setOpen}
+        />
+      </Box>
+    </Grid2>
+  );
+}
+
+export default GameCard;
