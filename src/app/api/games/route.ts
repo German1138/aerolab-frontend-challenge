b0daa9dd107de37e521386002cbcfa812c76ac75
German1138 <germancourvoisier1138@gmail.com>
2024-12-30 09:43:32 -0300

    refactor: :boom: Now getGames brings the "collected games" information
    This was changed due to the fact that we don't need to display games that are not part of our collection in the home page of the website, if not the ones that are part of our collection saved in the LocalStorage.

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -1,18 +1,39 @@
 import axios from "axios";
+import { NextRequest } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const slugs = req?.nextUrl?.searchParams?.get("slugs");
+  const filter = req?.nextUrl?.searchParams?.get("filter");
+
+  const slugsArray = slugs?.split(",");
+
+  const slugConditions = slugsArray
+    ?.map((slug) => `slug = "${slug}"`)
+    .join(" | ");
+
+  const BODY = `fields created_at,
+                name,
+                slug,
+                cover.url,
+                cover.image_id;
+                sort release_dates ${filter === "newest" ? "desc" : "asc"};
+                where ${slugConditions};
+                limit 50;`;
+
   try {
-    const { data } = await axios.post(
-      "https://api.igdb.com/v4/games",
-      "fields checksum,created_at,name,platforms,slug,updated_at,url,cover.*;",
-      {
-        headers: {
-          Accept: "application/json",
-          Authorization: process.env.AUTHORIZATION,
-          "Client-ID": process.env.CLIENT_ID,
-        },
-      }
-    );
+    const { data } = await axios.post("https://api.igdb.com/v4/games", BODY, {
+      headers: {
+        Accept: "application/json",
+        Authorization: process.env.AUTHORIZATION,
+        "Client-ID": process.env.CLIENT_ID,
+      },
+    });
+
+    if (filter === "last-added") {
+      data.sort(
+        (a, b) => slugsArray.indexOf(b.slug) - slugsArray.indexOf(a.slug)
+      );
+    }
 
     return new Response(JSON.stringify(data));
   } catch (error) {
