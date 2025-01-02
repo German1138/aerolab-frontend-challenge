ce8519eb12a8edd1f449a95ad160ffd441ba2329
German1138 <germancourvoisier1138@gmail.com>
2025-01-02 19:57:21 -0300

    feat: :sparkles: Searchbar fully functional
    Every bug is solved. Now it's possible to search any game and it will redirect to the detail page.

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,12 +1,18 @@
-"use client";
-
-import { Autocomplete, Box, InputAdornment, TextField } from "@mui/material";
+import {
+  Autocomplete,
+  Box,
+  InputAdornment,
+  TextField,
+  Typography,
+  CircularProgress,
+  debounce,
+} from "@mui/material";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import axios from "axios";
 import customImageUrl from "@/utils/customImageUrl";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface Option {
   id: string;
@@ -28,54 +34,87 @@ const fetchSearchResults = async (query: string): Promise<Option[]> => {
 };
 
 const Searchbar = () => {
+  const router = useRouter();
+
   const [options, setOptions] = useState<Option[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const handleInputChange = async (
-    event: React.ChangeEvent<HTMLInputElement>
+  const handleInputChange = useCallback(
+    debounce(async (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value;
+      setSearchTerm(value);
+      setError(null);
+      setLoading(true);
+
+      if (value.length >= 3) {
+        try {
+          const results = await fetchSearchResults(value);
+          if (results.length === 0) {
+            setError("No results found.");
+          }
+          setOptions(results);
+        } catch (err) {
+          console.error("Error fetching results.", err);
+          setOptions([]);
+        }
+      } else {
+        setOptions([]);
+      }
+      setLoading(false);
+    }, 500),
+    []
+  );
+
+  const handleOptionSelect = (
+    event: React.SyntheticEvent<Element, Event>,
+    value: string | Option | null
   ) => {
-    const value = event.target.value;
-    setSearchTerm(value);
-
-    if (value.length >= 3) {
-      const results = await fetchSearchResults(value);
-      setOptions(results);
-    } else {
-      setOptions([]);
+    if (value) {
+      if (typeof value === "string") {
+        return null;
+      } else {
+        console.log(`Opción seleccionada: ${JSON.stringify(value.slug)}`);
+        console.log(`Opción seleccionada: ${value.name}`);
+        router.push(`/detail/${value.slug}`);
+      }
     }
   };
 
-  const renderOption = (
-    props: React.HTMLAttributes<HTMLAnchorElement>,
-    option: Option
-  ) => (
-    <Link
-      {...props}
-      href={`/detail/${option.slug}`}
-      key={option.id}
-      style={{ display: "flex", alignItems: "center" }}
-    >
-      <img
-        src={customImageUrl("micro", option.cover?.image_id)}
-        alt={option.name}
-        style={{ width: 30, height: 30, marginRight: 10 }}
-      />
-      {option.name}
-    </Link>
-  );
-
   return (
-    <form style={{ width: "100%", margin: "auto" }}>
-      <Autocomplete
-        freeSolo
-        options={options}
-        inputValue={searchTerm}
-        onInputChange={(_, newInputValue) => setSearchTerm(newInputValue)}
-        getOptionLabel={(option) =>
-          typeof option === "string" ? option : option.name || ""
-        }
-        renderOption={renderOption}
-        renderInput={(params) => (
+    <Autocomplete
+      freeSolo
+      handleHomeEndKeys
+      autoHighlight
+      options={options}
+      inputValue={searchTerm}
+      onInputChange={(_, newInputValue) => setSearchTerm(newInputValue)}
+      onChange={handleOptionSelect}
+      getOptionLabel={(option) =>
+        typeof option === "string" ? option : option.name || ""
+      }
+      renderOption={(props, option) => {
+        const { key, ...restProps } = props;
+        return (
+          <li
+            key={key}
+            {...restProps}
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <Image
+              src={customImageUrl("micro", option.cover?.image_id || "")}
+              alt={option.name}
+              width={30}
+              height={30}
+              style={{ marginRight: 10 }}
+            />
+            {option.name}
+          </li>
+        );
+      }}
+      renderInput={(params) => (
+        <Box>
           <TextField
             {...params}
             placeholder="Search games..."
@@ -91,9 +130,19 @@ const Searchbar = () => {
               ),
             }}
           />
-        )}
-      />
-    </form>
+          {loading && (
+            <Box display="flex" justifyContent="center" marginTop={1}>
+              <CircularProgress size={24} />
+            </Box>
+          )}
+          {error && (
+            <Typography variant="body2" color="error" style={{ marginTop: 5 }}>
+              {error}
+            </Typography>
+          )}
+        </Box>
+      )}
+    />
   );
 };
 
