7945963e322e49bfe57ce5ee41c7349d767ed246
German1138 <germancourvoisier1138@gmail.com>
2025-01-03 09:56:38 -0300

    refactor: :recycle: Removed filters from Home page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,26 @@ import { Container, Typography } from "@mui/material";
 import Searchbar from "@/components/Searchbar/Searchbar";
 import GameCardContainer from "@/components/GameCardContainer/GameCardContainer";
 
-import { useState } from "react";
-
-import Filters from "@/components/Filters/Filters";
-
 export default function Home() {
-  const [filter, setFilter] = useState<string>("last-added");
-
   return (
-    <Container>
-      <Typography variant="h1" sx={{ fontSize: "20px", fontWeight: "600" }}>
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        padding: "30px 15px",
+      }}
+    >
+      <Typography
+        variant="h1"
+        sx={{ fontSize: "20px", fontWeight: "600", color: "#6727A6" }}
+      >
         Gaming Haven Z
       </Typography>
 
       <Searchbar />
 
-      <Typography variant="h3" sx={{ fontSize: "20px", fontWeight: "600" }}>
-        Saved Games
-      </Typography>
-
-      <Filters filter={filter} setFilter={setFilter} />
-
-      <GameCardContainer filter={filter} />
+      <GameCardContainer />
     </Container>
   );
 }
