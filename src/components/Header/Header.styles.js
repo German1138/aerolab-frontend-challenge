07e8d5b8f1e7e5dbc6351d143020a008847180a7
German1138 <germancourvoisier1138@gmail.com>
2025-01-03 22:43:17 -0300

    style: :art: Header component styles

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,25 @@
+export const container = {
+  display: { xs: "block", sm: "flex" },
+  justifyContent: "space-between",
+};
+
+export const goBackBtn = {
+  color: "#6727A6",
+  fontWeight: "600",
+  fontSize: "16px",
+  textTransform: "capitalize",
+  marginBottom: { xs: "15px", sm: 0 },
+};
+
+export const secondaryContainer = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
+};
+
+export const titleStyle = {
+  textAlign: { xs: "start", sm: "center" },
+  fontSize: "20px",
+  fontWeight: "600",
+  color: "#6727A6",
+};
