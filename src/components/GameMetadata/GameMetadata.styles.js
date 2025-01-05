a34aaa8b3072abbc1d3ae4a18127a8698c77b3d7
German1138 <germancourvoisier1138@gmail.com>
2025-01-04 23:00:37 -0300

    style: :art: GameMetadata component styles

---
+++ src/components/GameMetadata/GameMetadata.styles.js
@@ -0,0 +1,7 @@
+export const container = { display: "flex", gap: "10px", flexWrap: "wrap" };
+
+export const chipStyles = { width: "fit-content", padding: "0 10px" };
+
+export const labelStyles = { fontSize: "14px", color: "#6727A6" };
+
+export const infoStyles = { fontSize: "16px", color: "#3C1661" };
