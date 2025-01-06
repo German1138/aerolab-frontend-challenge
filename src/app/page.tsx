1c6c313706dc321f1558678f15533a99255bfed3
German1138 <germancourvoisier1138@gmail.com>
2025-01-06 11:04:24 -0300

    style: :recycle: Reordered components to display background correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,19 @@ import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "20px",
-        padding: "30px 15px",
-      }}
-    >
+    <>
       <Header />
 
-      <GameCardContainer />
-    </Container>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          padding: "0 15px 30px 15px",
+        }}
+      >
+        <GameCardContainer />
+      </Container>
+    </>
   );
 }
