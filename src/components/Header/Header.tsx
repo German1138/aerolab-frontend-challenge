2c8b3c3b0517d92877c28251e8d5d2f4c349d737
German1138 <germancourvoisier1138@gmail.com>
2025-01-06 11:11:30 -0300

    feat: :sparkles: Responsive background with SVGs
    Now we will see both decorations, background linear gradient and svg "wasd" drawing, depending on our viewport width size.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
-import React from "react";
-import Searchbar from "../Searchbar/Searchbar";
-import { useRouter } from "next/navigation";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-
+import { Box, Button, Typography, useMediaQuery } from "@mui/material";
 import {
   container,
   goBackBtn,
@@ -11,31 +6,89 @@ import {
   titleStyle,
 } from "./Header.styles";
 
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Image from "next/image";
+import React from "react";
+import Searchbar from "../Searchbar/Searchbar";
+import { useRouter } from "next/navigation";
+
 function Header({ goBackButton = false }) {
   const router = useRouter();
+  const isNotMobile = useMediaQuery("(min-width:768px)");
+
+  const handleImage = () => {
+    if (isNotMobile) {
+      return (
+        <Image
+          src="/wasd-desktop.svg"
+          alt="wasd header decoration"
+          width={1390.8}
+          height={176.79}
+          layout="responsive"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        />
+      );
+    } else {
+      return (
+        <Image
+          src="/wasd-mobile.svg"
+          alt="wasd header decoration"
+          width={196.21}
+          height={73.9}
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+          }}
+        />
+      );
+    }
+  };
 
   if (goBackButton)
     return (
-      <Box sx={container}>
-        <Button
-          startIcon={<ArrowBackIcon />}
-          onClick={() => router.back()}
-          sx={goBackBtn}
-        >
-          Back
-        </Button>
-
-        <Searchbar />
+      <Box
+        sx={{
+          background: "linear-gradient(#FF00AE29, #FFFFFF )",
+          padding: "30px 15px 50px 15px",
+          width: "100%",
+          display: { xs: "block", sm: "flex" },
+          justifyContent: "center",
+        }}
+      >
+        <Box sx={container}>
+          <Button
+            startIcon={<ArrowBackIcon />}
+            onClick={() => router.back()}
+            sx={goBackBtn}
+          >
+            Back
+          </Button>
+          <Searchbar />
+        </Box>
+        {handleImage()}
       </Box>
     );
 
   return (
-    <Box sx={secondaryContainer}>
+    <Box
+      sx={{
+        ...secondaryContainer,
+        background: "linear-gradient(#FF00AE29, #FFFFFF )",
+        padding: "30px 15px 50px 15px",
+      }}
+    >
       <Typography variant="h1" sx={titleStyle}>
         Gaming Haven Z
       </Typography>
-
       <Searchbar />
+
+      {handleImage()}
     </Box>
   );
 }
