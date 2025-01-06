f752b53f664f316bb742ab743ea178a4f2dbaa5c
German1138 <germancourvoisier1138@gmail.com>
2025-01-06 11:15:30 -0300

    feat: :sparkles: Added view screenshot modal
    It can be closed with and X button. There's a bug on the Next Image that give us pixelated images, need to see how to fix that.

--- src/components/MediaGallery/MediaGallery.tsx
+++ src/components/MediaGallery/MediaGallery.tsx
@@ -1,13 +1,22 @@
-import { Box, Typography, useMediaQuery } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  Modal,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import React, { useState } from "react";
 import { mediaContainer, subTitle } from "./MediaGallery.styles";
 
+import CloseIcon from "@mui/icons-material/Close";
 import { IGameProps } from "@/app/interfaces";
 import Image from "next/image";
-import React from "react";
 import customImageUrl from "@/utils/customImageUrl";
 
 function MediaGallery({ game }: IGameProps) {
   const isNotMobile = useMediaQuery("(min-width:1024px)");
+  const [selectedImage, setSelectedImage] = useState("");
+  const [open, setOpen] = useState(false);
 
   return (
     <>
@@ -18,17 +27,70 @@ function MediaGallery({ game }: IGameProps) {
         {game.screenshots.map((element) => {
           return (
             <Image
-              onClick={() => console.log("✅✅✅")}
+              onClick={() => {
+                setOpen(true);
+                setSelectedImage(element.image_id);
+              }}
               key={element.id}
               src={customImageUrl("thumb", element.image_id)}
               width={isNotMobile ? 132 : 85}
               height={isNotMobile ? 132 : 85}
               alt={`${game.name} screenshot`}
-              style={{ borderRadius: "8px" }}
+              style={{ borderRadius: "8px", cursor: "pointer" }}
             />
           );
         })}
       </Box>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{
+          bgcolor: "#E5E5E5",
+          backdropFilter: "blur(10px)",
+          WebkitBackdropFilter: "blur(10px)",
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "90%",
+            maxWidth: "1200px",
+            backgroundColor: "#FFFFFF",
+            borderRadius: "8px",
+            boxShadow: 24,
+            outline: "none",
+            padding: 2,
+          }}
+        >
+          <IconButton
+            onClick={() => setOpen(false)}
+            sx={{
+              position: "absolute",
+              top: -16,
+              right: -16,
+              color: "#666666",
+              backgroundColor: "#FFFFFF",
+
+              ":hover": { backgroundColor: "#E5E5E5" },
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+
+          <img
+            src={customImageUrl("1080p", selectedImage)}
+            alt={`${game.name} screenshot`}
+            style={{
+              width: "100%",
+              height: "auto",
+              borderRadius: "10px",
+            }}
+          />
+        </Box>
+      </Modal>
     </>
   );
 }
