9e6244927b794e9f3755f653287f12decce3f51f
German1138 <germancourvoisier1138@gmail.com>
2025-01-06 19:54:28 -0300

    style: :lipstick: Added font weight

--- src/components/GameMetadata/GameMetadata.styles.js
+++ src/components/GameMetadata/GameMetadata.styles.js
@@ -2,6 +2,14 @@ export const container = { display: "flex", gap: "10px", flexWrap: "wrap" };
 
 export const chipStyles = { width: "fit-content", padding: "0 10px" };
 
-export const labelStyles = { fontSize: "14px", color: "#6727A6" };
+export const labelStyles = {
+  fontSize: "14px",
+  color: "#6727A6",
+  fontWeight: "500",
+};
 
-export const infoStyles = { fontSize: "16px", color: "#3C1661" };
+export const infoStyles = {
+  fontSize: "16px",
+  color: "#3C1661",
+  fontWeight: "500",
+};
