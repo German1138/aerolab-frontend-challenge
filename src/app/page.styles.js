7003ccfd99c9d92995433bb06ac33ce170d1c79d
German1138 <germancourvoisier1138@gmail.com>
2025-01-07 18:53:56 -0300

    style: :lipstick: Added reusable typographyLinearGradient

--- src/app/page.styles.js
+++ src/app/page.styles.js
@@ -4,3 +4,9 @@ export const container = {
   gap: "20px",
   padding: { xs: "0 15px 30px 15px", md: "0 15px 100px 15px" },
 };
+
+export const typographyLinearGradient = {
+  background: "linear-gradient(to right, #3C1661,#6727A6 )",
+  WebkitBackgroundClip: "text",
+  WebkitTextFillColor: "transparent",
+};
