edaa5d017f798a0505aa1ad3454f92c007754502
German1138 <germancourvoisier1138@gmail.com>
2025-01-07 18:08:43 -0300

    style: :lipstick: Added subContainer styles
    Also modified container and secondaryContainer

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,4 +1,12 @@
 export const container = {
+  background: "linear-gradient(#FF00AE29, #FFFFFF )",
+  padding: { xs: "30px 15px 50px 15px", md: "100px 15px 50px 15px" },
+  width: "100%",
+  display: { xs: "block", sm: "flex" },
+  justifyContent: "center",
+};
+
+export const subContainer = {
   width: { xs: "auto", sm: "728px" },
   padding: { xs: 0, md: "0 15px" },
   display: { xs: "block", sm: "flex" },
@@ -18,6 +26,8 @@ export const secondaryContainer = {
   display: "flex",
   flexDirection: "column",
   gap: "20px",
+  background: "linear-gradient(#FF00AE29, #FFFFFF )",
+  padding: { xs: "30px 15px 50px 15px", md: "100px 15px 50px 15px" },
 };
 
 export const titleStyle = {
