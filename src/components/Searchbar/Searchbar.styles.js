092cd3b28149aa2e29528b350dfb667e164c4207
German1138 <germancourvoisier1138@gmail.com>
2025-01-07 18:17:54 -0300

    style: :lipstick: Total overhaul

--- src/components/Searchbar/Searchbar.styles.js
+++ src/components/Searchbar/Searchbar.styles.js
@@ -1,26 +1,46 @@
 export const container = {
   display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
+  justifyContent: "center",
   width: "100%",
-  maxWidth: "360px",
-  margin: "0 10px",
+  zIndex: 1300,
+};
+
+export const autocompleteStyles = {
+  width: "100%",
+  maxWidth: "400px",
 };
 
 export const textField = {
-  backgroundColor: "#FFFFFF",
-  borderRadius: "100px",
   width: "100%",
-  border: "1px solid #C698B8",
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      border: "none",
-    },
-    "&.Mui-focused fieldset": {
-      border: "none",
-    },
-    "&:hover fieldset": {
-      border: "none",
-    },
-  },
+  maxWidth: "400px",
+  boxShadow: "#F2D0E766 0px 4px 16px 0px",
+
+  overflow: "hidden",
+};
+
+export const liImage = { marginRight: 10, borderRadius: "4px" };
+
+export const isSearchBar = {
+  backgroundColor: "#FFFFFF",
+  border: "1px solid #E7C0DB",
+};
+
+export const circularProgress = {
+  padding: "5px 35px",
+  backgroundColor: "white",
+  borderRadius: "0 0 8px 8px",
+  borderLeft: "1px solid #E7C0DB",
+  borderRight: "1px solid #E7C0DB",
+  borderBottom: "1px solid #E7C0DB",
+};
+
+export const errorMessage = {
+  color: "#D23F63",
+  padding: "5px 35px",
+  backgroundColor: "white",
+  fontWeight: 600,
+  borderRadius: "0 0 8px 8px",
+  borderLeft: "1px solid #E7C0DB",
+  borderRight: "1px solid #E7C0DB",
+  borderBottom: "1px solid #E7C0DB",
 };
