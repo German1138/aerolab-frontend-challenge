2b0af92a18c3f80d9c996a94fdfc86c6396f7437
German1138 <germancourvoisier1138@gmail.com>
2025-01-07 18:25:59 -0300

    refactor: :recycle: Removed comment

--- src/utils/customImageUrl.ts
+++ src/utils/customImageUrl.ts
@@ -1,4 +1,3 @@
 export default function customImageUrl(size: string, imageId: string) {
   return `https://images.igdb.com/igdb/image/upload/t_${size}/${imageId}.jpg`;
 }
-//`https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover?.image_id}.jpg`
