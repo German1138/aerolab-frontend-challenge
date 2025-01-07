406c230b7ecbbb8457708d42a92aaa32cbae9fde
German1138 <germancourvoisier1138@gmail.com>
2025-01-07 17:03:34 -0300

    style: :lipstick: Updated padding
    Now it's bigger depending on the dispositive use, this give us a better user experience in desktop.

--- src/app/detail/[id]/page.styles.js
+++ src/app/detail/[id]/page.styles.js
@@ -2,6 +2,6 @@ export const container = {
   display: "flex",
   flexDirection: "column",
   gap: "20px",
-  padding: "15px",
+  padding: { xs: "15px", md: "0 15px 100px 15px" },
   width: { xs: "auto", md: "728px" },
 };
