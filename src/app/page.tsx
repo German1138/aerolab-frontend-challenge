df84c94e069c6a03e661dd6452a9f54ac9135dbf
German1138 <germancourvoisier1138@gmail.com>
2025-01-07 16:58:58 -0300

    style: :art: Moved styles to dedicated file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,14 @@
 import { Container } from "@mui/material";
 import GameCardContainer from "@/components/GameCardContainer/GameCardContainer";
 import Header from "@/components/Header/Header";
+import { container } from "./page.styles";
 
 export default function Home() {
   return (
     <>
       <Header />
 
-      <Container
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "20px",
-          padding: "0 15px 30px 15px",
-        }}
-      >
+      <Container sx={container}>
         <GameCardContainer />
       </Container>
     </>
