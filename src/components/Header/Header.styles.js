a4cd4fbaf9213aa4d390379e96487219e62eadf8
German1138 <germancourvoisier1138@gmail.com>
2025-01-07 18:32:14 -0300

    style: :lipstick: Added logo styles

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -30,6 +30,39 @@ export const secondaryContainer = {
   padding: { xs: "30px 15px 50px 15px", md: "100px 15px 50px 15px" },
 };
 
+export const secondarySubContainer = {
+  display: "flex",
+  justifyContent: { xs: "start", sm: "center" },
+  gap: "20px",
+  alignItems: "center",
+};
+
+export const logoContainer = {
+  width: "30px",
+  height: "30px",
+  boxShadow: "0px 0px 20px 0px rgb(255 0 174 / 17%)",
+  background: "linear-gradient(to top right,#FF0AB1, #FF90DC)",
+  borderRadius: "8px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "1px",
+  zIndex: 1,
+};
+
+export const logoSubContainer = {
+  borderRadius: "7px",
+  backgroundColor: "transparent",
+  zIndex: 2,
+  minWidth: "28px",
+  background: "linear-gradient(to top,#ffe8fb, #FFFFFF)",
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
 export const titleStyle = {
   textAlign: { xs: "start", sm: "center" },
   fontSize: "20px",
