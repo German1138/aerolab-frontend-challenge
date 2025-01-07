e87e8da79ae5c77455291de34c4af712fece5ebc
German1138 <germancourvoisier1138@gmail.com>
2025-01-07 18:09:51 -0300

    style: :lipstick: Added modal styles

--- src/components/MediaGallery/MediaGallery.styles.js
+++ src/components/MediaGallery/MediaGallery.styles.js
@@ -6,3 +6,33 @@ export const mediaContainer = {
   flexWrap: "nowrap",
   gap: "10px",
 };
+
+export const modalStyles = {
+  bgcolor: "#E5E5E5",
+  backdropFilter: "blur(10px)",
+  WebkitBackdropFilter: "blur(10px)",
+};
+
+export const modalSubContainer = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  maxWidth: "900px",
+  backgroundColor: "#FFFFFF",
+  borderRadius: "8px",
+  boxShadow: 24,
+  outline: "none",
+  padding: 2,
+};
+
+export const btn = {
+  position: "absolute",
+  top: -16,
+  right: -16,
+  color: "#666666",
+  backgroundColor: "#FFFFFF",
+
+  ":hover": { backgroundColor: "#E5E5E5" },
+};
