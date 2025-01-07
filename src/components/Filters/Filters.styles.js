609ef6c7ee2aac97586eb1802eebb624a3059682
German1138 <germancourvoisier1138@gmail.com>
2025-01-07 17:49:07 -0300

    style: :lipstick: Added floating filters styles

--- src/components/Filters/Filters.styles.js
+++ src/components/Filters/Filters.styles.js
@@ -12,3 +12,19 @@ export const btnStylesMobile = {
   textTransform: "capitalize",
   padding: "4.75px 12px",
 };
+
+export const floatingFiltersContainer = {
+  position: "fixed",
+  top: 20,
+  left: "50%",
+  transition: "opacity 0.3s ease, transform 0.3s ease",
+  zIndex: 1300,
+  display: "flex",
+  maxHeight: "40px",
+  textWrap: "nowrap",
+  backgroundColor: "#ffffffba",
+  borderRadius: "100px",
+  padding: "4px",
+  boxShadow: "none",
+  backdropFilter: "blur(10px)",
+};
